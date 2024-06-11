6e428cc28eec2a3935bd118177f37aed4ed60163
UBC Student <2860128966@qq.com>
2024-06-11 12:32:19 -0400

    add find by email

--- demo/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ demo/src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByEmail(String email);
 }
