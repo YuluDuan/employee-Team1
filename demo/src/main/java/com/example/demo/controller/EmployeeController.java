677c2a7e783f48e8a68e92b7e521f255805d5305
allant <allan.zf.tan@gmail.com>
2024-06-12 11:17:34 -0400

    created employee controller

---
+++ demo/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeRepository repository;
+
+    @GetMapping("/employee/{email}")
+    @ResponseBody
+    public List<Employee> findEmployee(@PathVariable String email) {
+        return repository.findByEmail(email);
+    }
+}
