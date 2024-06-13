9d15d5be85b179f43b5d57b773d393826caf2255
UBC Student <2860128966@qq.com>
2024-06-13 09:38:31 -0400

    update test data

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into employee (id, first_name, last_name, email)
 values (1, 'Rachel', 'Lee', 'rachel.lee@citi.com');
 
+insert into employee (id, first_name, last_name, email)
+values (2, 'Anne', 'Chulukov', 'anne.chulukov@citi.com');
+
